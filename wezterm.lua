dc119b5358bcf8c7f5cb888e9397732b48ef2be3
Suryakant Sharma <suryasharma2901@gmail.com>
2025-03-28 16:56:42 +0530

    initial commit

---
+++ wezterm.lua
@@ -0,0 +1,20 @@
+local wezterm = require('wezterm')
+local config = {
+    font = wezterm.font 'FiraMono Nerd Font Mono',
+    color_scheme = 'Tokyo Night',
+    font_size = 18,
+    inactive_pane_hsb = {
+        saturation = 0.3,
+        brightness = 0.3,
+        window_background_opacity = 9.0
+    },
+    enable_tab_bar = false,
+    window_decorations = "RESIZE",
+    window_padding = {
+        left = 0,
+        right = 0,
+        top = 0,
+        bottom = 0,
+    }
+}
+return config
